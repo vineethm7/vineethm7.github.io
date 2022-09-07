fd93c86df9579da3927b83498c310db46a3a08e1
VinMyn7 <vineethmyneedi123@gmail.com>
2022-09-07 12:03:37 -0500

    first commit
    Website for HW01

---
+++ js/app.js
@@ -0,0 +1,48 @@
+function push(id) {
+    // Update Title in Window's Tab
+    document.title = id;
+    // Finally push state change to the address bar
+    let url = null;
+    switch (id) {
+        case  "Visualization":
+            url = "./components/data.html"
+            break;
+        case "SVG" :
+            url = "./components/svg.html"
+            break;
+        case "Reference":
+            url = "./components/reference.html"
+            break;
+        case "Home":
+            url = "./components/home.html"
+            break;        
+        default:
+            break;
+    }
+    fetch(url).then((response) => response.text()).then(
+        (html) => {
+            $("#content").html(html)
+        }
+    )
+}
+window.onload = event => {
+    // Add history push() event when boxes are clicked
+    fetch("./components/home.html").then((response) => response.text()).then(
+        (html) => {
+            $("#content").html(html)
+        }
+    )
+
+    $("#data").on("click", function () {
+        push("Visualization")
+    })
+    $("#svg").on("click", function() {
+        push("SVG")
+    })
+    $("#reference").on("click", function() {
+        push("Reference")
+    })
+    $("#home").on("click", function() {
+        push("Home")
+    })
+}
